c9638d3f46a3cc6b3cbf3b94eb36c6546a8b1caf
hkalodner <harry.kalodner@gmail.com>
2017-09-19 16:22:17 -0400

    Add package script header

--- src/blocksci/script.hpp
+++ src/blocksci/script.hpp
@@ -9,5 +9,12 @@
 #ifndef script_h
 #define script_h
 
+#include <blocksci/scripts/scriptsfwd.hpp>
+#include <blocksci/scripts/script_access.hpp>
+#include <blocksci/scripts/pubkey_script.hpp>
+#include <blocksci/scripts/multisig_script.hpp>
+#include <blocksci/scripts/scripthash_script.hpp>
+#include <blocksci/scripts/nulldata_script.hpp>
+#include <blocksci/scripts/nonstandard_script.hpp>
 
 #endif /* script_h */
