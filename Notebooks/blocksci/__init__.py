a672ba62e268f6fa0af47ad6324e0941b6d61f0d
André Birke <abirke@campus.uni-paderborn.de>
2017-10-06 16:37:59 +0200

    fix parameters of mapreduce_block_ranges calls
    Change the order of positional arguments to mapreduce_block_ranges

--- Notebooks/blocksci/__init__.py
+++ Notebooks/blocksci/__init__.py
@@ -48,14 +48,14 @@ def mapreduce_blocks(chain, mapFunc, reduceFunc, init, start=None, end=None, cpu
     """
     def mapRangeFunc(blocks):
         return reduce(reduceFunc, (mapFunc(block) for block in blocks), init)
-    return mapreduce_block_ranges(chain, mapRangeFunc, start, end, reduceFunc, init, cpu_count)
+    return mapreduce_block_ranges(chain, mapRangeFunc, reduceFunc, init, start, end, cpu_count)
 
 def mapreduce_txes(chain, mapFunc, reduceFunc, init,  start=None, end=None, cpu_count=psutil.cpu_count()):
     """Initialized multithreaded map reduce function over a stream of transactions
     """
     def mapRangeFunc(blocks):
         return reduce(reduceFunc, (mapFunc(tx) for block in blocks for tx in block))
-    return mapreduce_block_ranges(chain, mapRangeFunc, start, end, reduceFunc, init, cpu_count)
+    return mapreduce_block_ranges(chain, mapRangeFunc, reduceFunc, init, start, end, cpu_count)
 
 
 def map_blocks(self, blockFunc, start = None, end = None, cpu_count=psutil.cpu_count()):
