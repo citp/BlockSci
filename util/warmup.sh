4dd7359228e4645b672f6856ca819eee4fef2743
hkalodner <harry.kalodner@gmail.com>
2017-12-13 14:30:22 -0500

    Change location of warmup files

--- util/warmup.sh
+++ util/warmup.sh
@@ -6,7 +6,7 @@ fio --filename=/home/ubuntu/bitcoin/chain/sequence_data.dat --rw=read --bs=128k 
 fio --filename=/home/ubuntu/bitcoin/chain/tx_hashes.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/chain/coinbases.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 
-touch /home/ubuntu/BlockSci/Notebooks/TX_DATA_HEATED
+touch /home/ubuntu/BlockSci/TX_DATA_HEATED
 
 fio --filename=/home/ubuntu/bitcoin/scripts/pubkey_script.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/scripts/multisig_script_data.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
@@ -17,9 +17,9 @@ fio --filename=/home/ubuntu/bitcoin/scripts/null_data_script_data.dat --rw=read 
 fio --filename=/home/ubuntu/bitcoin/scripts/null_data_script_index.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/scripts/scripthash_script.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 
-touch /home/ubuntu/BlockSci/Notebooks/SCRIPT_DATA_HEATED
+touch /home/ubuntu/BlockSci/SCRIPT_DATA_HEATED
 
 fio --filename=/home/ubuntu/bitcoin/hashIndex.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/addressesDb.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 
-touch /home/ubuntu/BlockSci/Notebooks/INDEX_DATA_HEATED
+touch /home/ubuntu/BlockSci/INDEX_DATA_HEATED
