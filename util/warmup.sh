0d4de340ce09c91aa51bd7597333d948bbdd35a5
hkalodner <harry.kalodner@gmail.com>
2017-12-13 16:39:24 -0500

    Change warmup order

--- util/warmup.sh
+++ util/warmup.sh
@@ -1,11 +1,6 @@
 fio --filename=/home/ubuntu/bitcoin/chain/block.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/chain/tx_index.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 fio --filename=/home/ubuntu/bitcoin/chain/tx_data.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-fio --filename=/home/ubuntu/bitcoin/chain/sequence_index.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-fio --filename=/home/ubuntu/bitcoin/chain/sequence_data.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-fio --filename=/home/ubuntu/bitcoin/chain/tx_hashes.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-fio --filename=/home/ubuntu/bitcoin/chain/coinbases.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
-
 touch /home/ubuntu/BlockSci/TX_DATA_HEATED
 
 fio --filename=/home/ubuntu/bitcoin/scripts/pubkey_script.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
@@ -23,3 +18,8 @@ fio --filename=/home/ubuntu/bitcoin/hashIndex.dat --rw=read --bs=128k --iodepth=
 fio --filename=/home/ubuntu/bitcoin/addressesDb.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
 
 touch /home/ubuntu/BlockSci/INDEX_DATA_HEATED
+
+fio --filename=/home/ubuntu/bitcoin/chain/sequence_index.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
+fio --filename=/home/ubuntu/bitcoin/chain/sequence_data.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
+fio --filename=/home/ubuntu/bitcoin/chain/tx_hashes.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
+fio --filename=/home/ubuntu/bitcoin/chain/coinbases.dat --rw=read --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize
