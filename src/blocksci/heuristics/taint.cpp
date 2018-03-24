90252185406c09016dff06f9b3434e2fb81926a4
hkalodner <harry.kalodner@gmail.com>
2018-03-24 17:51:23 -0400

    Change getHaircutTainted from recursive to iterative

--- src/blocksci/heuristics/taint.cpp
+++ src/blocksci/heuristics/taint.cpp
@@ -11,19 +11,22 @@
 namespace blocksci { namespace heuristics {
     std::unordered_map<Output, uint64_t> getHaircutTainted(const Output &output, uint64_t taintedValue) {
         std::unordered_map<Output, uint64_t> outputs;
-        auto spendingTx = output.getSpendingTx();
-        if (spendingTx) {
-            auto totalOut = static_cast<double>(totalOutputValue(*spendingTx));
-            for (auto spendingOut : spendingTx->outputs()) {
-                auto percentage = static_cast<double>(spendingOut.getValue()) / totalOut;
-                auto value = static_cast<uint64_t>(percentage * static_cast<double>(taintedValue));
-                auto subTainted = getHaircutTainted(spendingOut, value);
-                for (auto pair : subTainted) {
-                    outputs[pair.first] += pair.second;
+        std::vector<std::pair<Output, uint64_t>> outputsToCheck;
+        outputsToCheck.emplace_back(output, taintedValue);
+        while (!outputsToCheck.empty()) {
+            auto tainted = outputsToCheck.back();
+            outputsToCheck.pop_back();
+            auto spendingTx = tainted.first.getSpendingTx();
+            if (spendingTx) {
+                auto totalOut = static_cast<double>(totalOutputValue(*spendingTx));
+                for (auto spendingOut : spendingTx->outputs()) {
+                    auto percentage = static_cast<double>(spendingOut.getValue()) / totalOut;
+                    auto value = static_cast<uint64_t>(percentage * static_cast<double>(tainted.second));
+                    outputsToCheck.emplace_back(spendingOut, value);
                 }
+            } else {
+                outputs[tainted.first] += tainted.second;
             }
-        } else {
-            outputs[output] += taintedValue;
         }
         return outputs;
     }
