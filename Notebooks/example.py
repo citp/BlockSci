6f209024e61eb364f08456117b0d55a1c24b379d
Daniel Feher <dfeher.mail@gmail.com>
2018-07-01 13:45:14 +0200

    Update example.py

--- Notebooks/example.py
+++ Notebooks/example.py
@@ -1,18 +1,21 @@
 import blocksci
 import os
 
-chain = blocksci.Blockchain("../../../zcash-testdata") #the directory of your parsed zcash data
+chain = blocksci.Blockchain("../../../zcash-data") # Or your custom directory of your parsed zcash data
 
 in_z_addresses = 0
 num_of_txes = 0
 num_of_jstxes = 0
+num_of_joinsplits = 0
 num_of_ins = 0
 num_of_outs = 0
-
+val_of_vpubolds = 0
+val_of_vpubnews = 0
 
 for blk in chain:
 	for tx in blk:
 		num_of_txes += 1
+		num_of_joinsplits += tx.joinsplits
 		if tx.is_joinsplit:
 			num_of_jstxes += 1
 			in_z_addresses += tx.sum_vpubold - tx.sum_vpubnew
@@ -20,9 +23,15 @@ for blk in chain:
 			num_of_ins += 1
 		for outs in tx.outs:
 			num_of_outs += 1
+		for olds in tx.vpubold:
+			val_of_vpubolds += olds
+		for news in tx.vpubnew:
+			val_of_vpubnews += news
+			
 
 print('Number of transactions: ' + str(num_of_txes))
 print('Of that hidden transactions: ' + str(num_of_jstxes))
+print('Number of joinsplits: ' + str(num_of_joinsplits))
 print('Number of transaction inputs: ' + str(num_of_ins))
 print('Number of transaction outputs: ' + str(num_of_outs))
 print('amount of Zatoshis in Z addresses: ' + str(in_z_addresses))
