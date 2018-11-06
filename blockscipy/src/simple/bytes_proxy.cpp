967ef3fad6fcb0f68e30b873d635f5f7a60ff59c
Harry Kalodner <harry.kalodner@gmail.com>
2018-11-06 00:33:07 -0500

    Add startswith and endswith to bytes proxy

--- blockscipy/src/simple/bytes_proxy.cpp
+++ blockscipy/src/simple/bytes_proxy.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "simple_proxies.hpp"
+#include "proxy_apply_py.hpp"
 #include "proxy/range.hpp"
 #include "proxy/basic.hpp"
 #include "proxy/optional.hpp"
@@ -15,7 +16,26 @@
 #include <blocksci/cluster/cluster.hpp>
 #include <blocksci/address/equiv_address.hpp>
 
-void addBytesProxyMethods(AllProxyClasses<pybind11::bytes> &cls) {
+namespace py = pybind11;
+
+struct AddBytesMethods {
+    template <typename FuncApplication>
+    void operator()(FuncApplication func) {
+    	using namespace blocksci;
+	    func(method_tag, "startswith", +[](const py::bytes &bytes, const std::string &str) -> bool {
+			auto bytesString = static_cast<std::string>(bytes);
+			return bytesString.find(str, 0) == 0;
+		}, "Returns true if the byte string has the given prefix");
+		func(method_tag, "endswith", +[](const py::bytes &bytes, const std::string &str) -> bool {
+			auto bytesString = static_cast<std::string>(bytes);
+		return bytesString.size() >= str.size() &&
+			   bytesString.compare(bytesString.size() - str.size(), str.size(), str) == 0;
+		}, "Returns true if the byte string has the given suffix");
+    }
+};
+
+void addBytesProxyMethods(AllProxyClasses<py::bytes> &cls) {
 	cls.applyToAll(AddProxyMethods{});
 	addProxyOptionalMethods(cls.optional);
+	applyMethodsToProxy(cls.base, AddBytesMethods{});
 }
