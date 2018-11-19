86d1cda42dbe2b5c3888edaf463dd5163e469fd9
Harry Kalodner <harry.kalodner@gmail.com>
2018-11-19 12:12:46 -0500

    Make take_while return a proxy/iterator instead of a list

--- blockscipy/src/generic_proxy/optional.cpp
+++ blockscipy/src/generic_proxy/optional.cpp
@@ -12,7 +12,7 @@
 #include "proxy_utils.hpp"
 #include "caster_py.hpp"
 
-void addOptionalProxyMethods(pybind11::class_<OptionalProxy, GenericProxy> &cl, pybind11::module &m) {
+void addOptionalProxyMethods(pybind11::class_<OptionalProxy, GenericProxy> &cl, pybind11::module &) {
 	addOptionalProxyMapMethods(cl);
 	
 	cl
@@ -21,26 +21,6 @@ void addOptionalProxyMethods(pybind11::class_<OptionalProxy, GenericProxy> &cl, 
 			return std::forward<decltype(val)>(val).has_value();
 		});
 	})
-	;
-
-	m
-    .def("take_while", [](OptionalProxy &body, BlocksciTypeVariant &init) -> pybind11::list {
-        auto generic = body.getGeneric();
-        pybind11::list vec;
-        ranges::optional<BlocksciType> v = BlocksciType{init};
-        while(true) {
-
-        	addToList(vec, *v);
-        	auto anyVal = v->toAny();
-        	auto nextVal = generic(anyVal);
-        	if (nextVal) {
-        		v.emplace(*nextVal);
-        	} else {
-        		break;
-        	}
-        }
-        return vec;
-    })
 	;
 }
 
--- blockscipy/src/proxy/proxy_functions_impl.hpp
+++ blockscipy/src/proxy/proxy_functions_impl.hpp
@@ -13,6 +13,33 @@
 #include "caster_py.hpp"
 
 template <typename T>
+class take_while_range : public ranges::view_facade<take_while_range<T>> {
+    friend ranges::range_access;
+
+    ranges::optional<T> v;
+    Proxy<ranges::optional<T>> body;
+
+    const T &read() const { 
+        return *v;
+    }
+
+    bool equal(ranges::default_sentinel) const {
+        return !v;
+    }
+
+    void next() { 
+        v = body(*v); 
+    }
+
+public:
+    take_while_range() : body(std::function<ranges::optional<T>(std::any &)>{}, createProxyTypeInfo<ranges::optional<T>>()) {};
+    explicit take_while_range(
+        const Proxy<ranges::optional<T>> &bodyFunc,
+        const ranges::optional<T> &initialVal
+    ) : v(initialVal), body(bodyFunc) {}
+};
+
+template <typename T>
 void addProxyFunctionsT(pybind11::module &m) {
     m
     .def("conditional", [](const Proxy<bool> &cond, const Proxy<T> &p1, const Proxy<T> &p2) -> Proxy<T> {
@@ -42,11 +69,23 @@ void addProxyFunctionsT(pybind11::module &m) {
 }
 
 template <typename T>
-void addProxyFunctions(pybind11::module &, pybind11::module &pm) {
+void addProxyFunctions(pybind11::module &m, pybind11::module &pm) {
     addProxyFunctionsT<T>(pm);
     addProxyFunctionsT<ranges::optional<T>>(pm);
     addProxyFunctionsT<RawIterator<T>>(pm);
     addProxyFunctionsT<RawRange<T>>(pm);
+
+    m
+    .def("take_while", [](Proxy<ranges::optional<T>> &body, Proxy<ranges::optional<T>> &initF) -> Proxy<RawIterator<T>> {
+        return {std::function<RawIterator<T>(std::any &)>{[body, initF](std::any &v) -> RawIterator<T> {
+            return RawIterator<T>{take_while_range<T>{std::move(body), initF(v)}};
+        }}, initF.sourceType};
+        
+    })
+    .def("take_while", [](Proxy<ranges::optional<T>> &body, ranges::optional<T> &init) -> Iterator<T> {
+        return RawIterator<T>{take_while_range<T>{std::move(body), init}};
+    })
+    ;
 }
 
 #endif /* proxy_functions_impl_hpp */
