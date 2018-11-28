d41fcf00af938a21ad4e22036b0ef71f0910d5da
Malte Möser <ilovegit@maltemoeser.de>
2018-11-28 09:26:19 -0500

    Fix parser run on Travis, also parse BCH regtest

--- .travis/test_parser.sh
+++ .travis/test_parser.sh
@@ -1,3 +1,5 @@
 cd test
-blocksci_parser regtest.json generate-config bitcoin_regtest bitcoin_regtest --disk files/regtest/
-blocksci_parser regtest.json update
+blocksci_parser btc.json generate-config bitcoin_regtest bitcoin_regtest --disk files/btc/regtest/
+blocksci_parser btc.json update
+blocksci_parser bch.json generate-config bitcoin_cash_regtest bitcoin_cash_regtest --disk files/bch/regtest/
+blocksci_parser bch.json update
