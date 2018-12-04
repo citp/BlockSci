3c4050a8a1f957986d4f71f4868d618d3588a308
Malte Möser <ilovegit@maltemoeser.de>
2018-12-04 15:36:59 -0500

    Fix client_change_address_behavior test case
    Also adds a negative testcase

--- test/blockscipy/test_change.py
+++ test/blockscipy/test_change.py
@@ -71,10 +71,16 @@ def test_address_reuse_change(chain, json_data):
         assert idx == list(result)[0].index
 
 
-def test_address_client_behavior_change(chain, json_data):
-    # TODO: write a correct test case
-    # changing the generated pattern will break some of the regression tests
-    pass
+def test_client_behavior_change(chain, json_data):
+    for i in range(3):
+        txid = json_data["change-client-behavior-tx-{}".format(i)]
+        tx = chain.tx_with_hash(txid)
+
+        result = blocksci.heuristics.change.client_change_address_behavior(tx)
+        assert 1 == len(result)
+
+        idx = json_data["change-client-behavior-position-{}".format(i)]
+        assert idx == list(result)[0].index
 
 
 def test_no_change(chain, json_data):
@@ -86,3 +92,26 @@ def test_no_change(chain, json_data):
 
             result = blocksci.heuristics.change.none(tx)
             assert 0 == len(result)
+
+
+def test_negative_testcase(chain, json_data):
+    tx = chain.tx_with_hash(json_data['change-negative-testcase-tx'])
+    empty_heuristics = [
+        blocksci.heuristics.change.peeling_chain,
+        blocksci.heuristics.change.optimal_change,
+        blocksci.heuristics.change.address_reuse,
+        blocksci.heuristics.change.legacy,
+        blocksci.heuristics.change.none
+    ]
+    for h in empty_heuristics:
+        result = h(tx)
+        assert 0 == len(result)
+
+    full_heuristics = [
+        blocksci.heuristics.change.address_type,
+        blocksci.heuristics.change.locktime,
+        blocksci.heuristics.change.client_change_address_behavior
+    ]
+    for h in full_heuristics:
+        result = h(tx)
+        assert 2 == len(result)
