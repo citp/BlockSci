9ebecc9ecd1d0bc0070c6f1ddf13e7fb1a197c9a
Malte Möser <ilovegit@maltemoeser.de>
2019-01-22 11:44:37 -0500

    Generate docs (no old docs or deploy yet)

--- .travis/build_ubuntu_python.sh
+++ .travis/build_ubuntu_python.sh
@@ -1,3 +1,4 @@
 sudo apt install python3-dev python3-pip
 sudo pip3 install -U pip setuptools
 sudo pip3 install pytest pytest-regtest pytz tzlocal
+sudo pip3 install sphinx sphinx_rtd_theme
---
+++ .travis/generate_docs.sh
@@ -0,0 +1,5 @@
+cd "$TRAVIS_BUILD_DIR"
+cd docs
+python3 -msphinx -M html . _output -a -E
+
+cd "$TRAVIS_BUILD_DIR"
