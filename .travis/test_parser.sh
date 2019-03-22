90c442af9ea1727521279d5b00d683b122f0cd8b
Malte Möser <ilovegit@maltemoeser.de>
2019-03-22 14:06:07 -0400

    Test litecoin parsing

--- .travis/test_parser.sh
+++ .travis/test_parser.sh
@@ -4,4 +4,6 @@ blocksci_parser btc.json generate-config bitcoin_regtest bitcoin_regtest --disk 
 blocksci_parser btc.json update
 blocksci_parser bch.json generate-config bitcoin_cash_regtest bitcoin_cash_regtest --disk files/bch/regtest/
 blocksci_parser bch.json update
+blocksci_parser ltc.json generate-config litecoin_regtest litecoin_regtest --disk files/ltc/regtest/
+blocksci_parser ltc.json update
 cd "$TRAVIS_BUILD_DIR"
