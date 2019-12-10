c15cab5b8f1f0f8f53bfcd53bcf0bfb010569b96
Malte Möser <ilovegit@maltemoeser.de>
2019-12-10 16:43:07 -0500

    ci: move env variables to .travis.yml

--- .travis/compile_ubuntu_cpp.sh
+++ .travis/compile_ubuntu_cpp.sh
@@ -2,7 +2,7 @@ set -ev
 cd "$TRAVIS_BUILD_DIR"
 mkdir -p release
 cd release
-CC=gcc-7 CXX=g++-7 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 mtime_cache ../**/*.{%{cpp}} -c ../.mtime_cache/cache.json
 make -j 2
 make install
