55bd9a6318525a5b012e3c78d8d5c9c0784de332
Malte Möser <ilovegit@maltemoeser.de>
2020-04-23 15:59:14 -0400

    fix: don't delete address based on iterator
    addressesToSearch.begin() is not consistent across architectures

--- src/internal/address_index.cpp
+++ src/internal/address_index.cpp
@@ -216,7 +216,7 @@ namespace blocksci {
                 }
             }
             searchedAddresses.insert({address.scriptNum, dedupType(address.type)});
-            addressesToSearch.erase(addressesToSearch.begin());
+            addressesToSearch.erase(address);
         }
         return searchedAddresses;
     }
