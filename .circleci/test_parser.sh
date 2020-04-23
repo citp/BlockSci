00eb8ec802688625db1f7b19a1fdf5cf4444a1b0
Malte Möser <ilovegit@maltemoeser.de>
2020-04-23 14:52:21 -0400

    ci: run index checks on circleci

--- .circleci/test_parser.sh
+++ .circleci/test_parser.sh
@@ -3,13 +3,13 @@ cd test
 blocksci_parser btc.json generate-config bitcoin_regtest bitcoin_regtest --disk files/btc/regtest/
 blocksci_parser btc.json doctor
 blocksci_parser btc.json update
-blocksci_check_integrity btc.json
+blocksci_check_integrity btc.json -i
 blocksci_parser bch.json generate-config bitcoin_cash_regtest bitcoin_cash_regtest --disk files/bch/regtest/
 blocksci_parser bch.json doctor
 blocksci_parser bch.json update
-blocksci_check_integrity bch.json
+blocksci_check_integrity bch.json -i
 blocksci_parser ltc.json generate-config litecoin_regtest litecoin_regtest --disk files/ltc/regtest/
 blocksci_parser ltc.json doctor
 blocksci_parser ltc.json update
-blocksci_check_integrity ltc.json
+blocksci_check_integrity ltc.json -i
 cd ..
